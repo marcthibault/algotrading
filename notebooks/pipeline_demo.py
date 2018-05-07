cced0ffd83f2e52d82f910e0dc7c97d7fceec0f0
Marc Thibault <marcthib@stanford.edu>
2018-05-07 13:29:34 -0700

    Enable save before plot; preventing from crashing.

--- notebooks/pipeline_demo.py
+++ notebooks/pipeline_demo.py
@@ -8,7 +8,7 @@ raw_data = pd.read_csv('../data/data_f2009.csv', parse_dates=['date']).set_index
 data = raw_data.sort_index()
 
 # %% filter data
-nb_stocks = 500
+nb_stocks = 1
 data['filter'] = VolumeFilter(data, nb_stocks).get_filter()
 
 # %% create signal
@@ -22,6 +22,9 @@ data_perf['perf'] = data_perf.position * data_perf.future_perf_1d
 data_perf['to'] = data_perf.position.groupby(level=1).diff().fillna(0).abs()
 data_perf['not'] = data_perf.position.fillna(0).abs()
 
+data_perf.to_csv("data.csv")
+data_perf.to_csv("data_perf.csv")
+
 # %% perfs
 print('Sharpe: ' + "{0:.3f}".format(16 * data_perf.perf.groupby(
     level=0).sum().mean() / data_perf.perf.groupby(level=0).sum().std()))
@@ -30,5 +33,3 @@ print('rbt: ' + "{0:.3f}".format(100 * data_perf.perf.groupby(
 print('holding: ' + "{0:.2f}".format(2 * data_perf['not'].groupby(
     level=0).sum().sum() / data_perf.to.groupby(level=0).sum().sum()))
 data_perf.perf.groupby('date').sum().cumsum().plot(figsize=(12, 7))
-
-data_perf.to_csv("output.csv")
