9d5ae2671b7b0141ec37400bb6bc36df24d24713
Mael Trean <mael.trean@gmail.com>
2018-05-08 13:52:42 -0700

    beta

--- scripts/signals/betaresidual.py
+++ scripts/signals/betaresidual.py
@@ -15,22 +15,22 @@ class BetaResidual(Signal):
 
     def _compute(self):
         if self.past:
-            data['perf'] = data.past_perf_1d
-            data['idx_perf'] = data.loc[data['filter'] == 1].groupby(
+            self.data['perf'] = self.data.past_perf_1d
+            self.data['idx_perf'] = self.data.loc[data['filter'] == 1].groupby(
                 'date').past_perf_1d.transform(np.mean)
         else:
-            data['perf'] = data.future_perf_1d
-            data['idx_perf'] = data.loc[data['filter'] == 1].groupby(
+            self.data['perf'] = self.data.future_perf_1d
+            self.data['idx_perf'] = self.data.loc[data['filter'] == 1].groupby(
                 'date').future_perf_1d.transform(np.mean)
 
         rw = self.rw
 
-        temp_cov = data.groupby(level=1, group_keys=False)[['perf', 'idx_perf']].rolling(rw).cov()
+        temp_cov = self.data.groupby(level=1, group_keys=False)[['perf', 'idx_perf']].rolling(rw).cov()
         temp_cov = temp_cov.reset_index(level=[0], drop=True).sort_index().loc(
             axis=0)[:, :, 'perf'].reset_index(level=2, drop=True).idx_perf.rename('cov')
 
-        temp_var = data.groupby(level=1, group_keys=False)['idx_perf'].rolling(rw).var()
+        temp_var = self.data.groupby(level=1, group_keys=False)['idx_perf'].rolling(rw).var()
         temp_var = temp_var.reset_index(level=[0], drop=True).sort_index().rename('var')
 
-        data['beta'] = temp_cov / temp_beta
-        self.signal = data.perf - data.beta * data.idx_perf
+        self.data['beta'] = temp_cov / temp_var
+        self.signal = self.data.perf - self.data.beta * self.data.idx_perf
