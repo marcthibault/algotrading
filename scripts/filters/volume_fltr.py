6ed165f4738dd3f201c05a3ab3db522e3b939ddf
Mael Trean <mael.trean@gmail.com>
2018-05-13 15:36:19 -0700

    volume filter fixed

--- scripts/filters/volume_fltr.py
+++ scripts/filters/volume_fltr.py
@@ -1,5 +1,6 @@
 from .filter import Filter
 import numpy as np
+import pandas as pd
 
 
 class VolumeFilter(Filter):
@@ -9,7 +10,15 @@ class VolumeFilter(Filter):
         self.last_days = last_days
 
     def _compute(self):
-        past_volume = self.data.groupby(level=1).adj_volume.rolling(self.last_days, min_periods=1).mean().fillna(0)
+        vol_dolls = self.data.adj_volume*self.data.adj_close
+        past_volume = vol_dolls.groupby(level=1).rolling(self.last_days, min_periods=1).mean().fillna(0)
         past_volume.index = past_volume.index.droplevel(0)
-        self.data['past_volume'] = past_volume
-        self.filter = self.data.past_volume.groupby(level=0).apply(lambda x: np.argsort(-x) < self.nb_stocks)
+        self.data['past_volume'] = vol_dolls
+        def f(x):
+            y = np.argsort(-x)
+            res = np.zeros(len(x))
+            res[y[:self.nb_stocks]] = 1
+
+            return pd.Series(res, index=x.index)
+
+        self.filter = self.data.past_volume.groupby(level=0).apply(f)
