84233c5ec82279960a49cf3bdf88e6a7f360a997
Maxime Rivet <maxime.rivet92@gmail.com>
2018-06-03 22:34:44 -0700

    Push __init__ for filters

--- scripts/filters/__init__.py
+++ scripts/filters/__init__.py
@@ -1,3 +1,5 @@
 from . import *
 from .volume_fltr import VolumeFilter
 from .fixed_volume_fltr import FixedVolumeFilter
+from .fixed_list_filter import FixedListFilter
+from .slice_filter import SliceFilter
